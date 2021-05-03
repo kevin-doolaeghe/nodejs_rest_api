4d98fa8b193c7c2fc5e0279cb72b17334ed66428
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2021-05-03 23:00:05 +0200

    REST API Example

--- README.md
+++ README.md
@@ -10,11 +10,20 @@ Installation des dépendances utiles pour Node JS :
 
 ```
 sudo apt install nodejs npm
-sudo npm install express jshint nodemon
+sudo npm install express jshint joi
+sudo npm install -g nodemon
 ```
 
 ## Démarrage
 
+Démarrage classique avec `node` :
+
 ```
 node app.js
+```
+
+Prise en compte des modifications avec `nodemon` :
+
+```
+nodemon app.js
 ```
--- app.js
+++ app.js
@@ -1,18 +1,66 @@
+const Joi = require('joi'); // Input validation
+const express = require('express'); // Easier requests
+const app = express();
 
-const http = require('http');
+app.use(express.json());
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('Hello World!');
-        res.end();
-    }
+const courses = [];
 
-    if (req.url === '/api/courses') {
-        res.write(JSON.stringify([1, 2, 3]));
-        res.end();
-    }
+app.get('/', (req, res) => {
+    res.send('Course management');
 });
 
-server.listen(3000);
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
 
-console.log('Listening on port 3000...')
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course not found..');
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course not found..');
+
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course not found..');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().max(30).min(3).required()
+    });
+
+    return schema.validate(course);
+}
+
+// Variable environnement PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
